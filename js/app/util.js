03368608f8823c422dd14a2169005d1015b2251c
no-dap <somniholic@mathpresso.com>
2021-07-06 21:12:31 +0900

    Escape Latin also

--- js/app/util.js
+++ js/app/util.js
@@ -3723,7 +3723,7 @@ define([
     };
 
     let decodeUnicode = (unicodeString) => {
-        const r = /\\u([\d\w]{4})/gi;
+        const r = /(\\u([\d\w]{4}))|(\\x[\d\w]{2})/gi;
         unicodeString = unicodeString.replace(r, function (match, grp) {
             return String.fromCharCode(parseInt(grp, 16));
         });
